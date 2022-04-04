be485cf518e53ef63fa2e2c3100175905c578203
heeJNa <jim2353@naver.com>
2022-04-04 16:46:58 +0900

    commit

--- src/main/java/com/sist/dao/MyPageDAO.java
+++ src/main/java/com/sist/dao/MyPageDAO.java
@@ -1,9 +1,13 @@
 package com.sist.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.sist.mapper.MyPageMapper;
 
 @Repository
 public class MyPageDAO {
-	
+	@Autowired
+	private MyPageMapper mapper;
 	
 }
