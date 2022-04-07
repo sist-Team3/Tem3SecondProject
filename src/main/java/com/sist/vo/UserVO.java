9919860811def76714e9f80c2d8df37a75031957
heeJNa <jim2353@naver.com>
2022-04-07 14:59:39 +0900

    UserVO 복

--- src/main/java/com/sist/vo/UserVO.java
+++ src/main/java/com/sist/vo/UserVO.java
@@ -5,28 +5,12 @@ import lombok.Setter;
 import lombok.ToString;
 import java.util.*;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-
-import jakarta.validation.constraints.NotBlank;
-
 @Getter @Setter @ToString
 public class UserVO {
 	private String id;
-	@Email
 	private String email;
-	@NotBlank
 	private String password;
-	@NotBlank
-	private String name;
-	private String user_role;
-	@NotEmpty
-	private String address1; 
-	private String address2;
-	@NotBlank
-	private String dbday;
-	private int phone;
-	private int postcode;
-
+	private String name, userRole,address1,address2,dbday;
+	private Integer phone,postcode;
 	private Date birth,registed_at;
 }
